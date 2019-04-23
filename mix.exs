d0c328d86abc3451e6049ff6547755050fab3b82
jnylen <me@jnylen.nu>
2019-04-23 13:05:46 +0200

    Up version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Xler.MixProject do
   def project do
     [
       app: :xler,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       compilers: [:rustler] ++ Mix.compilers(),
